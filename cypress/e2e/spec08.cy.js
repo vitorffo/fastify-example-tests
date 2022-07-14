0c7082c266110c3ff64aceff67333126df919745
Vitor <fernandofernandesvitor@gmail.com>
2022-07-13 22:40:18 -0400

    Lesson 8 updated with bahmutov's import style

--- cypress/e2e/spec08.cy.js
+++ cypress/e2e/spec08.cy.js
@@ -1,7 +1,11 @@
 /// <reference types="cypress" />
 
 // import the fixture JSON data from the file "../fixtures/apple.json"
-import fruit from '../fixtures/apple.json'
+import { fruit } from '../fixtures/apple.json'
+/***
+ Using { fruit } imports the value from the key right the way instead of
+ the whole object so i don't have to use fruit.fruit
+***/
 
 it('imports the fixture from JSON file', () => {
   // print the imported fruit to the Command Log
@@ -15,5 +19,5 @@ it('imports the fixture from JSON file', () => {
   cy.visit('/')
   // confirm the fruit from the fixture is shown on the page
   // https://on.cypress.io/contains
-  cy.contains('#fruit', fruit.fruit).should('be.visible')
+  cy.contains('#fruit', fruit).should('be.visible')
 })
