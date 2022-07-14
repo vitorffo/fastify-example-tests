128aa67d59b3329daf262f199b43d3d256d3db0f
Vitor <fernandofernandesvitor@gmail.com>
2022-07-13 21:48:17 -0400

    Lesson 6 - Done

--- cypress/e2e/spec06.cy.js
+++ cypress/e2e/spec06.cy.js
@@ -13,4 +13,25 @@ it('shows the loading element then fruit from a fixture', () => {
   //
   // confirm there is no element with the text "loading..."
   // https://on.cypress.io/should
+
+  cy.intercept('GET', '/fruit', {fixture: 'apple.json'}).as('fruit')
+
+  cy.visit('/')
+
+  cy.get('#fruit')
+    .should('contain.text', 'apple')
+})
+
+it('shows the loading element then fruit from a fixture [BAH]', () => {
+  // intercept the GET call to /fruit with fixture "apple.json"
+  cy.intercept('GET', '/fruit', { fixture: 'apple.json' })
+  // visit the site
+  cy.visit('/')
+  // confirm the "loading..." text is shown
+  cy.contains('#fruit', 'loading...').should('be.visible')
+  // confirm the "apple" text is shown
+  cy.contains('#fruit', 'apple').should('be.visible')
+  // confirm there is no element with the text "loading..."
+  cy.contains('#fruit', 'loading...').should('not.exist')
+
 })
