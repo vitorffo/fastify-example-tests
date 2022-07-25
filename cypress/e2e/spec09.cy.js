28a67f05ee00964d9cd90781ed9442ce230fe79e
Vitor <fernandofernandesvitor@gmail.com>
2022-07-24 22:23:13 -0400

    lessons 9 and 10 completed

--- cypress/e2e/spec09.cy.js
+++ cypress/e2e/spec09.cy.js
@@ -9,5 +9,59 @@ it('returns different fruits', () => {
   //
   // confirm it shows "apple"
   // reload the site
+  // confirm it shows "grapes
+  const fruits = ["apple", "grapes"]
+
+  cy.intercept({
+    method: 'GET',
+    url: '/fruit',
+    times: 1
+  },
+    { fruit: fruits[1] }
+  )
+
+  cy.intercept({
+      method: 'GET',
+      url: '/fruit',
+      times: 1,
+    },
+    { fruit: fruits[0] }
+  )
+
+  cy.visit('/')
+  cy.contains('#fruit', fruits[0]).should('be.visible')
+  cy.reload()
+  cy.contains('#fruit', fruits[1]).should('be.visible')
+})
+
+/// <reference types="cypress" />
+
+it('returns different fruits [BAH]', () => {
+  // stub the /fruit endpoint to return "apple" on the first visit
+  // stub the /fruit endpoint to return "grapes" on the second visit
+  cy.intercept(
+    {
+      method: 'GET',
+      url: '/fruit',
+      times: 1,
+    },
+    { fruit: 'grapes' },
+  )
+  cy.intercept(
+    {
+      method: 'GET',
+      url: '/fruit',
+      times: 1,
+    },
+    { fruit: 'apple' },
+  )
+
+  // visit the site
+  cy.visit('/')
+  // confirm it shows "apple"
+  cy.contains('#fruit', 'apple').should('be.visible')
+  // reload the site
+  cy.reload()
   // confirm it shows "grapes"
+  cy.contains('#fruit', 'grapes').should('be.visible')
 })
--- cypress/e2e/spec10.cy.js
+++ cypress/e2e/spec10.cy.js
@@ -10,4 +10,39 @@ it('shows the loading element with Promise.delay', () => {
   // make sure the loading element is visible
   // from the request spy, get the response body
   // and confirm the fruit returned by the server is visible
+
+  cy.intercept('GET', '/fruit', (req) => {
+    return Cypress.Promise.delay(2000)
+  }).as('fruit')
+
+  cy.visit('/')
+
+  cy.get('#fruit').should('have.text', 'loading...')
+
+  cy.wait('@fruit').then( (interception) => {
+    const fruit = interception.response.body.fruit
+
+    cy.log(`Response: ${fruit}`)
+
+    cy.get('#fruit').should('have.text', fruit)
+
+  })
+
+})
+
+it('bahmutovs solution', () => {
+  cy.intercept('GET', '/fruit', (req) => {
+    return Cypress.Promise.delay(2000)
+  }).as('fruit')
+
+  cy.visit('/')
+
+  cy.contains('#fruit', 'loading...')
+
+  cy.wait('@fruit')
+    .its('response.body.fruit')
+    .then(cy.log)
+    .then((fruit) => {
+      cy.contains('#fruit', fruit).should('be.visible')
+    })
 })
