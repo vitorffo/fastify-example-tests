9b1fe817f8821b7db306b7bcc0fa332e32948ff3
Vitor <fernandofernandesvitor@gmail.com>
2022-07-26 22:47:24 -0400

    Lesson 14 done

--- cypress/e2e/spec14.cy.js
+++ cypress/e2e/spec14.cy.js
@@ -8,4 +8,46 @@ it('reloads the page until it shows Bananas', () => {
   //   reload the page
   //   check again
   // Tip: use recursion
+  let reloadPage = () => {
+    cy.get('#fruit')
+      .should('not.have.text', 'loading...')
+      .invoke('text')
+      .then((text) => {
+        if (text === 'Bananas') {
+          cy.log(`${text} finally found`)
+        }else{
+          cy.reload()
+          cy.wait(1000)
+          reloadPage()
+        }
+      })
+  }
+
+  cy.visit('/')
+  reloadPage()
+})
+
+
+it('reloads the page until it shows Bananas. [Bah] solution', () => {
+  // visit the page
+  cy.visit('/')
+  // if it shows the fruit "Bananas", stop
+  // else
+  //   wait for 1 second for clarity
+  //   reload the page
+  //   check again
+  function checkFruit() {
+    cy.get('#fruit')
+      .should('not.have.text', 'loading...')
+      .invoke('text')
+      .then((fruit) => {
+        if (fruit === 'Bananas') {
+          cy.log('Bananas!')
+        } else {
+          cy.wait(1000)
+          cy.reload().then(checkFruit)
+        }
+      })
+  }
+  checkFruit()
 })
