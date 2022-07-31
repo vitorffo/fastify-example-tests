8244556a012b8760fbcadd6634f3a253c15667d2
Vitor <fernandofernandesvitor@gmail.com>
2022-07-31 00:21:45 -0400

    Lesson 15 done

--- cypress/e2e/spec15.cy.js
+++ cypress/e2e/spec15.cy.js
@@ -1,14 +1,68 @@
 /// <reference types="cypress" />
 
 it('finds all fruits', () => {
+  let uniqueFruits = new Set()
+  let fruitsList = []
   // visit the page
+  cy.visit('/')
   // keep getting the fruit from the page
   // and storing it in a Set object
   // and reloading the page
   // until we see the fruit we have already added
+  let getAllFruits = () => cy.get('#fruit')
+    .should('not.have.text', 'loading...')
+    .invoke('text')
+    .then( (text) => {
+      cy.log(text)
+      if(uniqueFruits.has(text)) {
+        cy.log(`${fruitsList}`)
+      } else {
+        fruitsList.push(text)
+        uniqueFruits.add(...fruitsList)
+        cy.reload()
+        getAllFruits()
+      }
+    })
   // print the collected list of fruits
   // check its length against the expected value
+  getAllFruits()
+  cy.wrap(fruitsList).should('have.length', 5)
+
 })
+
+it('finds all fruits [BAH]', () => {
+  // visit the page
+  cy.visit('/')
+  const fruits = new Set()
+
+  // keep getting the fruit from the page
+  // and storing it in a Set object
+  // and reloading the page
+  // until we see the fruit we have already added
+  function getFruit() {
+    cy.get('#fruit')
+      .should('not.have.text', 'loading...')
+      .invoke('text')
+      .then((fruit) => {
+        cy.log(fruit)
+        if (fruits.has(fruit)) {
+          // we are done
+          // print the collected list of fruits
+          // check its length against the expected value
+          const list = [...fruits].sort()
+          cy.log(list.join(', '))
+          expect(list).to.have.length(5)
+        } else {
+          fruits.add(fruit)
+          cy.wait(500)
+          cy.reload().then(getFruit)
+        }
+      })
+  }
+  getFruit()
+})
+
+
 
 // Bonus 2: use cypress-recurse to find all fruits
 // install the plugin cypress-recurse
