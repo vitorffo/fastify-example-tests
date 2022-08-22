713cd571b06e494226f312cccba3bc4bcd54d476
Vitor <fernandofernandesvitor@gmail.com>
2022-08-21 20:27:33 -0400

    Lesson 23 Done

--- cypress/e2e/spec23.cy.js
+++ cypress/e2e/spec23.cy.js
@@ -7,16 +7,25 @@ it('gets all interceptions', () => {
   cy.reload()
   cy.reload()
   // wait for the fruit to be shown on the page
+  cy.contains('#fruit', /^[A-Z]/)
   // need to get all network calls from the alias "fruit"
   // tip: use https://on.cypress.io/get command
   // like cy.get(alias.all)
-
   // confirm there are 4 responses
   // print the responses to the console using
   // cy.get(alias.all).then(console.log)
   // and inspect the available properties
-  //
   // if we use cy.wait(alias) then each response
+  cy.wait('@fruit')
+  cy.wait('@fruit')
+  cy.get('@fruit.all')
+    .then(console.log)
+    .should('have.length', 4)
+    .its(0)
+    .should('have.property', 'requestWaited', true)
   // will mark those intercepts as "requestWaited: true"
   // confirm this is true for the first request
+  cy.get('@fruit.all')
+    .its(2)
+    .should('have.property', 'requestWaited', false)
 })
