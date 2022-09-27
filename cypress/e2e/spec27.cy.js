2e9e1058a8f124384602209f7d7a6bc4e6e146dc
Vitor <fernandofernandesvitor@gmail.com>
2022-09-26 23:12:33 -0400

    Lesson 27 and 28, Done

--- cypress/e2e/spec27.cy.js
+++ cypress/e2e/spec27.cy.js
@@ -4,8 +4,13 @@ it('simulates the error status code', () => {
   // intercept the GET /fruit call and
   // return a response with status code 404
   // https://on.cypress.io/intercept
-  //
+  cy.intercept(
+    'GET', 
+    '/fruit',
+    { statusCode: 404 }
+    )
   // visit the site
-  //
+  cy.visit('/')
   // confirm the page has an element with text "HTTP error 404"
+  cy.contains('#fruit', 'HTTP error 404')
 })
--- cypress/e2e/spec28.cy.js
+++ cypress/e2e/spec28.cy.js
@@ -4,15 +4,23 @@ it('shows the network connection error', () => {
   // intercept the GET /fruit call and
   // force a network error
   // https://on.cypress.io/intercept
-  //
+  cy.intercept('GET', '/fruit', {
+     forceNetworkError: true })
   // visit the site and before load spy on
   // the window.console.error method
   // and give the spy an alias "logError"
   // https://on.cypress.io/visit and https://on.cypress.io/spy
-  //
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        cy.spy(win.console, 'error').as('logError')
+      }
+    })
   // confirm the console.error spy was called
   // with the expected message
   // "error fetching fruit"
-  //
+  cy.get('@logError').should(
+    'have.been.calledWith', 
+    'error fetching fruit')
   // confirm the page has an element with text "Failed to fetch"
+  cy.contains('#fruit', 'Failed to fetch')
 })
